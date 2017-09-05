2aff19c448e9d613c1022254b80c14d9ab6b8a06
IamEld3st <iameld3st@gmail.com>
2017-09-05 13:33:06 +0200

    Update getSkins.py

--- getSkins.py
+++ getSkins.py
@@ -4,63 +4,111 @@ import time
 from io import BytesIO
 from PIL import Image
 import requests
+from ratelimit import rate_limited
+import base64
+import json
 
 args = sys.argv[1:]
 
-def processSkin(skinHash, sType, path):
-    imageName = skinHash+".png"
+def getList(page):
+    url = "https://mcskinsearch.com/api/getUsers/"+str(page)
+    return requests.get(url)
+
+def getUUIDs(usernameList):
+    print("Getting UUIDs for "+str(len(usernameList))+" players...")
+    req = requests.post("https://api.mojang.com/profiles/minecraft", json=usernameList)
+    res = req.json()
+    print(len(res))
+    uuidList = []
+    for i in range(0,len(res)):
+        uuidList.append(res[i]['id'])
+    print("Got UUIDs for "+str(len(uuidList))+" players!")
+    return uuidList
+
+def getSkinUrl(uuid):
+    reqOK = False
+    while reqOK == False:
+        global userRaw
+        userRaw = requests.get("https://api.4lex.de/skin/"+uuid)
+        if userRaw.status_code == 200:
+            reqOK = True
+    userJson = userRaw.json()
+    decodedRes = json.loads(base64.b64decode(userJson['properties'][0]['value']))
+    return decodedRes['textures']['SKIN']['url']
+
+@rate_limited(1)
+def processSkin(uuid, sType, path):
+    imageName = uuid+".png"
     savePath = os.path.join(path, imageName)
-    url = "https://mcskinsearch.com/texture/skin/"+skinHash+".png"
-    req = requests.get(url)
+    skinUrl = getSkinUrl(uuid)
+    req = requests.get(skinUrl)
     img = BytesIO(req.content)
     try:
-        global imgIn
         imgIn = Image.open(img)
+        w, h = imgIn.size
+        if sType == "0":
+            if h == 32:
+                imgOut = Image.new("RGBA", (64, 64))
+                imgOut.paste(imgIn, (0, 0, 64, 32))
+                rl = imgIn.crop((0, 16, 16, 32))
+                imgOut.paste(rl, (16, 48, 32, 64))
+                ra = imgIn.crop((40, 16, 56, 32))
+                imgOut.paste(ra, (32, 48, 48, 64))
+                imgOut.save(savePath)
+            else:
+                imgIn.save(savePath)
+        if sType == "1":
+            if h == w:
+                imgOut = Image.new("RGBA", (64, 32))
+                box = (0, 0, 64, 32)
+                imgOut.paste(imgIn.crop(box), box)
+                imgOut.save(savePath)
+            else:
+                imgIn.save(savePath)
     except:
-        print("Image failed... Hash: "+skinHash)
+        print("Image failed... UUID: "+uuid)
 
-    w, h = imgIn.size
-    if sType == "0":
-        if h == 32:
-            imgOut = Image.new("RGBA", (64, 64))
-            imgOut.paste(imgIn, (0, 0, 64, 32))
-            rl = imgIn.crop((0, 16, 16, 32))
-            imgOut.paste(rl, (16, 48, 32, 64))
-            ra = imgIn.crop((40, 16, 56, 32))
-            imgOut.paste(ra, (32, 48, 48, 64))
-            imgOut.save(savePath)
-        else:
-            imgIn.save(savePath)
-    if sType == "1":
-        if h == w:
-            imgOut = Image.new("RGBA", (64, 32))
-            box = (0, 0, 64, 32)
-            imgOut.paste(imgIn.crop(box), box)
-            imgOut.save(savePath)
-        else:
-            imgIn.save(savePath)
-
-def getSkins(page, sType, path):
-    url = "https://mcskinsearch.com/api/getUsers/"+str(page)
-    req = requests.get(url)
+@rate_limited(1)
+def getUsernames(offset):
+    print("Attempting to get 20 usernames...")
+    responseList = []
+    gotOKResponse = False
+    page = offset+1
+    while gotOKResponse == False:
+        global req
+        req = getList(page)
+        if req.status_code == 200:
+            gotOKResponse = True
     jreq = req.json()
     reqList = jreq['list']
     for i in range(0, 20):
-         processSkin(reqList[i]['skinHash'], sType, path)
-    print("Page done.")
+        responseList.append(reqList[i]['username'])
+    print("Got "+str(len(responseList))+" usernames!")
+    return responseList
 
-print("Checking if path exist...")
-currDir = os.path.dirname(__file__)
-saveDir = os.path.join(currDir, args[2])
-if os.path.exists(saveDir):
-    print("Folder exist!")
-else:
-    print("Folder doesn't exist... Creating...")
-    os.makedirs(saveDir)
+def checkDirectory(path):
+    print("Checking if path exist...")
+    currDir = os.path.dirname(__file__)
+    saveDir = os.path.join(currDir, path)
+    if os.path.exists(saveDir):
+        print("Folder exist!")
+    else:
+        print("Folder doesn't exist... Creating...")
+        os.makedirs(saveDir)
+    return saveDir
+
+savePath = checkDirectory(args[2])
 skinReq = int(args[0]) // 20
-print("Will get "+str(skinReq)+" pages.")
+progress = 0
+startOffset = int(args[3])
 for i in range(0, skinReq):
-    print("Downloading page: "+str(i+1))
-    getSkins(i+1, args[1], saveDir)
-    time.sleep(1)
+    uuidList = getUUIDs(getUsernames(i))
+    for x in range(0, len(uuidList)):
+        y = x + startOffset
+        processSkin(uuidList[x], args[1], savePath)
+        if ((x+1) % 10) == 0:
+            progress += 10
+            if x != 0:
+                print("Progress: "+str(progress)+"/"+args[0])
+
 print("All done!")
